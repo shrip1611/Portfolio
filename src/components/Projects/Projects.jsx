ccf88df7328df09ef3e04dbd7835b351afd7c4cb
shrip1611 <shrirampandav16@gmail.com>
2026-03-16 13:15:58 +0530

    Added projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,13 +2,8 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
-
+import Restroapp from "../../Assets/Projects/Restro-app.png";
+import Portfolio from "../../Assets/Projects/Portfolio.png"
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -21,72 +16,37 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
+           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={Portfolio}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Portfolio"
+              description="Developed a personal portfolio website using React to showcase projects, technical skills, and professional information in a structured way. Implemented a projects section to display completed projects with descriptions, technologies used, and live project links. Used React component-based architecture to build reusable components. Created different sections such as Home, About, Projects, and Resume to present information clearly and improve navigation and also added download Functionality."
+              ghLink="https://github.com/shrip1611/Portfolio"
+              demoLink="https://shrip1611.github.io/Restro-app/"
             />
           </Col>
-
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={Restroapp}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="DocGrow"
+              description="Developed UI components using React.js and modern JavaScript (ES6+). Integrated REST APIs with the frontend using Axios for data fetching and submission. Implemented Axios interceptors for handling request/response processing, headers, and global error handling. Added request and response encryption/decryption to ensure secure communication with backend APIs. Developed real-time features using Socket.IO for live data updates between frontend and backend. Implemented role-based access control and permission management to restrict features based on user roles. Built dynamic forms and validation logic for creating and updating application data."
+              ghLink="https://github.com/itdocgrow/docgrow_react_dashboard/tree/dev"
             />
           </Col>
-
-          <Col md={4} className="project-card">
+         
+           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={Restroapp}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Restro-app"
+              description="Designed an interactive and user-friendly interface to make navigation simple and improve the overall user experience. Developed a hero section with a clear Call-to-Action (CTA) button to guide users toward key sections of the website. Implemented a filter-based menu system where users can view food items by categories such as All, Veg, and Non-Veg. Created a offer banner section to highlight special restaurant offers and promotions. Implemented aform submission feature using a POST request with JSONPlaceholder API to simulate backend form data submission. Added pagination in the menu section to display menu cards efficiently and improve performance when handling multiple items. Designed the layout to be fully responsive."
+              ghLink="https://github.com/shrip1611/Restro-app"
+              demoLink="https://shrip1611.github.io/Restro-app/"
             />
           </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col>
+          
         </Row>
       </Container>
     </Container>
